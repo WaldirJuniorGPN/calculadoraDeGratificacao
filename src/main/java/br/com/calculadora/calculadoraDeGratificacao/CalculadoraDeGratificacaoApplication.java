d41e702dffabc493814d7c738c27c7ea7128d94f
Waldir Júnior <waldirjunior.gpn@gmail.com>
2023-11-15 09:53:16 -0300

    Primeiro commit

---
+++ src/main/java/br/com/calculadora/calculadoraDeGratificacao/CalculadoraDeGratificacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.calculadora.calculadoraDeGratificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraDeGratificacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraDeGratificacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/calculadora/calculadoraDeGratificacao/CalculadoraDeGratificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.calculadora.calculadoraDeGratificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraDeGratificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
